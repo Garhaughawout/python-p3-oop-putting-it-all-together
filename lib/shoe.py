4429710df3e21f5b330fae385ebd2cda6526a02d
Garhaughawout <108907661+Garhaughawout@users.noreply.github.com>
2024-05-29 16:24:33 -0500

    All tests passing.
    all tests passing

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    @classmethod
+    def cobble(cls):
+        print("Your shoe is as good as new!")
+        cls.condition = "New"
+
+nike = Shoe("Nike", 10)
+nike.cobble()
+print(nike.condition)
